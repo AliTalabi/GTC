6cb81d1456c95799447d74aa12077b80fb663e97
Ali Talabi <ali.talabi.official@gmail.com>
2026-08-07 23:42:34 +0330

    perf: optimize player collision detection

--- game.py
+++ game.py
@@ -31,26 +31,30 @@ class Game:
 
         for block in self.block_list:
 
-            if self.player.entity.right == block.entity.left and (
-                    (block.entity.bottom >= self.player.entity.top >= block.entity.top) or
-                    (block.entity.bottom >= self.player.entity.bottom >= block.entity.top)
+            if (
+                    self.player.entity.right == block.entity.left
+                    and self.player.entity.top < block.entity.bottom
+                    and self.player.entity.bottom > block.entity.top
             ):
                 self.player.entity_blocked_directions.append('right')
 
-            if self.player.entity.left == block.entity.right and (
-                    (block.entity.bottom >= self.player.entity.top >= block.entity.top) or
-                    (block.entity.bottom >= self.player.entity.bottom >= block.entity.top)
+            if (
+                    self.player.entity.left == block.entity.right
+                    and self.player.entity.top < block.entity.bottom
+                    and self.player.entity.bottom > block.entity.top
             ):
                 self.player.entity_blocked_directions.append('left')
 
-            if self.player.entity.top == block.entity.bottom and (
-                    (block.entity.right >= self.player.entity.left >= block.entity.left) or
-                    (block.entity.right >= self.player.entity.right >= block.entity.left)
+            if (
+                    self.player.entity.top == block.entity.bottom
+                    and self.player.entity.left < block.entity.right
+                    and self.player.entity.right > block.entity.left
             ):
                 self.player.entity_blocked_directions.append('up')
 
-            if self.player.entity.bottom == block.entity.top and (
-                    (block.entity.right >= self.player.entity.left >= block.entity.left) or
-                    (block.entity.right >= self.player.entity.right >= block.entity.left)
+            if (
+                    self.player.entity.bottom == block.entity.top
+                    and self.player.entity.left < block.entity.right
+                    and self.player.entity.right > block.entity.left
             ):
                 self.player.entity_blocked_directions.append('down')
